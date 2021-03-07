afe7242a8495650348c8742f123a0bbd067a3650
Michel Megens <michel@michelmegens.net>
2021-03-07 01:32:53 +0100

    Update sensor mapping table.
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.SmartEnergy.Dsmr.Processor.Database/dbo/Tables/SensorMapping.sql
+++ SensateIoT.SmartEnergy.Dsmr.Processor.Database/dbo/Tables/SensorMapping.sql
@@ -2,7 +2,11 @@
 (
 	[Id]						INT				NOT NULL IDENTITY (1, 1) NOT FOR REPLICATION,
 	[SensorId]					VARCHAR(24)		NOT NULL,
+	[GasSensorId]				VARCHAR(24)		NOT NULL,
+	[ServiceName]				VARCHAR(32)		NOT NULL,
+	[Enabled]					BIT				NOT NULL DEFAULT(1)
 
 	CONSTRAINT [PK_SensorMapping] PRIMARY KEY NONCLUSTERED ([Id] ASC),
-	INDEX [IX_SensorMapping_SensorId] UNIQUE NONCLUSTERED ([SensorId])
+	INDEX [IX_SensorMapping_SensorId] UNIQUE NONCLUSTERED ([SensorId]),
+	INDEX [IX_SensorMapping_ServiceName] UNIQUE NONCLUSTERED ([ServiceName])
 )
