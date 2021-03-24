217e006a345d58a24a2f014dcd406215df1581b2
Michel Megens <michel@michelmegens.net>
2021-03-24 20:15:56 +0100

    project: db: migrate the database project
    Remove the database project, as this project has been moved to its own
    solution.
    
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- SensateIoT.SmartEnergy.Dsmr.Processor.Database/Security/db_processor.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE ROLE [db_processor];
-GO
-
-GRANT EXECUTE ON [dbo].[DsmrProcessor_FinalizeBatch] TO [db_processor]
-GO
-GRANT EXECUTE ON [dbo].[DsmrProcessor_SelectSensorMapping] TO [db_processor]
-GO
-GRANT EXECUTE ON [dbo].[DsmrProcessor_InsertDataPoint] TO [db_processor]
-GO
